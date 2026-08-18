93967a7361260e546e7663a3cd3031d96b005a2b
Pedro Guillemaut Despecher <pedrodespecher@Laptop-de-Pedro.local>
2026-08-18 17:12:36 -0300

    etapa 3: carga multiple con menu

--- Program.cs
+++ Program.cs
@@ -10,3 +10,40 @@ Console.Write("Ingrese Precio");
 decimal precio = Convert.ToDecimal(Console.ReadLine());
 Console.WriteLine($"Producto {Producto} Precio {precio}");
 
+decimal totalVenta = 0;
+int cantidadProductos = 0;
+string opcion;
+
+do
+{
+    Console.WriteLine("¿Qué desea hacer?");
+    Console.WriteLine("1 – Cargar un producto");
+    Console.WriteLine("2 – Cerrar la venta");
+    Console.Write("Seleccione una opción: ");
+    opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("Ingrese producto: ");
+            string producto = Console.ReadLine();
+
+            Console.Write("Ingrese precio: ");
+            decimal precio = Convert.ToDecimal(Console.ReadLine());
+
+            totalVenta += precio;
+            cantidadProductos++;
+            break;
+
+        case "2":
+            Console.WriteLine($"\nCantidad de productos: {cantidadProductos}");
+            Console.WriteLine($"Total a pagar: {totalVenta:C}");
+            break;
+
+        default:
+            Console.WriteLine("Opción inválida. Intente de nuevo.\n");
+            break;
+    }
+
+} while (opcion != "2");
+
